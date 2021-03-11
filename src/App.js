6fc436752641f5e5d28c5c929994da34c3abf2a2
Erikhvam <erikhvam@outlook.com>
2021-03-11 18:11:24 +0100

    cleaning up redundant lines

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from "react";
+import React from "react";
 
 import "./App.scss";
 import Navbar from "./components/Navbar/Navbar.js";
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -83,36 +83,3 @@ export const About = () => {
 };
 
 export default About;
-
-{
-	/* <h1>Curriculum Vitae</h1>
-					<br></br>
-					<div className="cv-wrapper">
-						<div className="utdanning">
-							<h2>Utdanning</h2>
-							<p>August 2007 - Juni 2008 Nesbru Videregående Skole</p>
-							<p>August 2010 - Juni 2011 Bjørknes Privatskole</p>
-						</div>
-						<br></br>
-						<div className="arbeidserfaring">
-							<h2>Arbeidserfaring</h2>
-							<p>1. sept 2009 – 1. mai 2010: Kasserer, Smart Club Slependen</p>
-							<p>
-								<p>
-									10. okt 2014 - 28. feb 2017: Seniorrådgiver, Webhelp Nordic
-								</p>
-								1. mars 2017 - 1. sept 2020: Senior Technical Consultant,
-								PasientSky AS
-							</p>
-						</div>
-						<br></br>
-						<br></br>
-						<h3>Annen erfaring</h3>
-						<p>April/Mai 2009: Dugnadsmedarbeider for ROK</p>
-						<p>Juli 2008: Festivalmedarbeider ved Stavernsfestivalen.</p>
-						<br></br>
-						<h3>Språk</h3>
-						<p>Norsk: morsmål</p>
-						<p>Engelsk: svært godt</p>
-					</div> */
-}
