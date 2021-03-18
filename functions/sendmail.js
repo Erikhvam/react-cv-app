9c70236c411c49ccfd480f0881c6a3b63a55fc81
Erikhvam <erikhvam@outlook.com>
2021-03-18 19:11:02 +0100

    .

--- functions/sendmail.js
+++ functions/sendmail.js
@@ -1,6 +1,8 @@
 const nodemailer = require("nodemailer");
 const { google } = require("googleapis");
 
+process.env.TZ = "Europe/Oslo";
+
 const CLIENT_ID =
 	"4332569409-ase0amv9h6pls3atvo89c12cgopak0oa.apps.googleusercontent.com";
 const CLIENT_SECRET = "ec1CVJhCO06Mtd8R_kEWIueu";
@@ -40,35 +42,26 @@ exports.handler = async (event) => {
 			},
 		});
 		console.log("hei");
-		transporter.sendMail(
+
+		const mailOptions = {
+			from: "erikhvamdev@gmail.com",
+			to: "erikhvamdev@gmail.com",
+			subject: `${data.subject}`,
+			text: `${data.message}`,
+			html: `
+			<h3>Epost fra ${data.name}. Epost: ${data.email}<h3>
+			<p>Emne: ${data.subject}<p>
+			<p>Beskjed: ${data.message}<p>
+			`,
+		};
+
+		const result = await transporter.sendMail(mailOptions);
+		console.log(result);
+		return (
+			null,
 			{
-				from: "erikhvamdev@gmail.com",
-				to: "erikhvamdev@gmail.com",
-				subject: `${data.subject}`,
-				text: `${data.message}`,
-				html: `
-				<h3>Epost fra ${data.name}. Epost: ${data.email}<h3>
-				<p>Emne: ${data.subject}<p>
-				<p>Beskjed: ${data.message}<p>
-				`,
-			},
-			function (error, info) {
-				if (error) {
-					console.log("Hallo");
-					return error;
-					console.log(error);
-				} else {
-					console.log("success");
-					return (
-						null,
-						{
-							statusCode: 200,
-							body: JSON.stringify({
-								result: "success",
-							}),
-						}
-					);
-				}
+				statusCode: 200,
+				body: JSON.stringify(result),
 			}
 		);
 	} catch (error) {
